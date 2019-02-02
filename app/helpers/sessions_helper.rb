5b55a3d9d55aefd1b0318bc778b2c7510c4cb975
y_hirotoshi <h.yanagi-95@outlook.jp>
2019-02-02 18:49:20 +0900

    Implement advanced login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,16 +4,36 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
   def log_out
+    current_user.forget
     session.delete(:user_id)
     @current_user = nil
   end
 
   def current_user
-    if session[:user_id]
-      User.find_by(id: session[:user_id])
+    if (user_id = session[:user_id]) # ログインしてたらこの分岐
+      @current_user ||= User.find_by(id: user_id)
+    # ログインしてないけど, cookie上に暗号化されてるuseridを読み出せたらこの分岐
+    elsif (user_id = cookies.signed[:user_id]) 
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token]) # ユーザとトークンでログイン認証
+        log_in user
+        @current_user = user
+      end
     end
-  end
+ end
 
   def logged_in?
     !current_user.nil?
