d9a5c1ce6e97a5d2326734cec001d49ef333ef5d
y_hirotoshi <h.yanagi-95@outlook.jp>
2019-02-11 00:53:50 +0900

    Add account activation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    redirect_to root_path
+    redirect_to root_path and return unless @user.activated?
   end
 
   def new
